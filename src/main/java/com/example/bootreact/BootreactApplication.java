41b7ad6b9fa3ad88a74b83e1851fb4531565b293
yoonhang <com717942@gmail.com>
2024-07-04 23:25:07 +0900

    1

---
+++ src/main/java/com/example/bootreact/BootreactApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootreactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bootreact/BootreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
